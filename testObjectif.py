8196faf58708c50487bace58a744d7ddb1eab887
AdamGreco <47328617+AdamGreco@users.noreply.github.com>
2019-02-21 11:53:49 +0100

    Add files via upload

---
+++ testObjectif.py
@@ -0,0 +1,29 @@
+'''
+Created on 20 fevr. 2019
+
+@author: Adam
+'''
+
+import unittest
+
+class testObjectif
+    def setUp (self):
+        self.a = objectif (1,1)
+
+    def test_point (self):
+        self.assertEqual(self.a.x,1)
+        self.assertEqual(self.a.y,1)
+        self.assertEqual(self.a.x,x1-30)
+        self.assertEqual(self.a.y,y1-30)
+
+    def testCentre (self):
+        self.assertEqual(self.a.centre,2)
+
+    if __name__==’__main__’:
+        unittest.main()
+
+    
+
+
+
+    
